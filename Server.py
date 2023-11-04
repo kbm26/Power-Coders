536ecd38d927a90cef41a873a6961fc1bdf931ac
Liam <liammaroney89@gmail.com>
2023-11-04 02:54:38 +0200

    Tonight

---
+++ Server.py
@@ -0,0 +1,27 @@
+import sqlite3
+import shutil
+import os
+
+from flask import Flask, render_template, redirect, request,send_file
+from werkzeug.utils import secure_filename
+app = Flask(__name__)
+
+path = os.getcwd()
+# UPLOAD_FOLDER = os.path.join(path, 'static/uploads')
+
+# if not os.path.isdir(UPLOAD_FOLDER):
+#     os.mkdir(UPLOAD_FOLDER)
+
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    with sqlite3.connect('database.db') as conn:
+        cursor = conn.cursor()
+        # cursor.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, firstname TEXT NOT NULL, lastname TEXT NOT NULL, email TEXT NOT NULL, contact_no TEXT NOT NULL, account_type TEXT NOT NULL)')
+        conn.commit()
+    return render_template('home.html')
+
+ 
+if __name__ == '__main__':
+    app.run(debug=True)
